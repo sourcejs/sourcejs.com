19f15f57065ecbcbd8a30e8b040dd27c89887d03
Robert Haritonov <r@rhr.me>
2015-05-28 00:30:01 +0200

    cleaning up configuration

--- options.js
+++ options.js
@@ -1,37 +1,9 @@
-// User options for overriding core options.js
-
 module.exports = {
-    // Restart app after changing core options
-    core: {
-        common: {
-            specFiles: ['index.src', 'index.jsx', 'index.jade', 'index.haml', 'index.md', 'readme.md', 'README.md', 'index.html']
-        }
-    },
-
-    // Run `grunt` after changing assets options. Or use`grunt watch-all`.
     assets: {
-        modulesEnabled : {
-            // Overriding example
-            // trimSpaces: true
-        },
-
         modulesOptions : {
             search: {
                 autoFocusOnNavigationPage: false
             }
-        },
-
-        // Plugins
-        pluginsEnabled : {
-            // user/plugins/counter/counter.js enabling example
-            // 'counter/counter': true
-        },
-
-        pluginsOptions : {
-            // Plugins options example
-            // somePlugin : {
-            //     someOption: true
-            // }
         }
     }
 };
